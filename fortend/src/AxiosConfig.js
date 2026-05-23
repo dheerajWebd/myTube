6127044a0d72c49091a0f33c5151e698e8a8b74b
dheeraj kumar dwivedi <dheerajdwivediweb@gmail.com>
2026-05-23 19:57:25 +0530

    fix: Update base API URL to use the production endpoint

--- fortend/src/AxiosConfig.js
+++ fortend/src/AxiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 "http://localhost:3000/myTube/requesst-singin"
 
 const base_API = axios.create({
-   baseURL: "http://localhost:5000",
+   baseURL: "https://mytube-backend-beta-v.onrender.com",
    // timeout: 10000,
    withCredentials: true,
 });
